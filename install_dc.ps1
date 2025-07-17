e070ce0b09304d5013d8d82cbd1e53d0145adc8d
mourad1223 <mjaadar1@myges.fr>
2025-07-17 16:46:44 +0200

    install_dc.ps1

--- install_dc.ps1
+++ install_dc.ps1
@@ -1,18 +1,55 @@
-#
-# Script Windows PowerShell pour le déploiement d’AD DS
-#
+<#
+.SYNOPSIS
+    Déploie un nouveau domaine Active Directory (AD DS) avec un contrôleur de domaine principal.
 
-Import-Module ADDSDeployment
-Install-ADDSForest `
--CreateDnsDelegation:$false `
--DatabasePath "C:\Windows\NTDS" `
--DomainMode "WinThreshold" `
--DomainName "neoinfra.fr" `
--DomainNetbiosName "NEOINFRA" `
--ForestMode "WinThreshold" `
--InstallDns:$true `
--LogPath "C:\Windows\NTDS" `
--NoRebootOnCompletion:$false `
--SysvolPath "C:\Windows\SYSVOL" `
--Force:$true
+.DESCRIPTION
+    Utilise le module ADDSDeployment pour installer un nouveau domaine dans une nouvelle forêt,
+    avec options de configuration personnalisables.
 
+.PARAMETER DomainName
+    Nom DNS complet du domaine (ex : neoinfra.fr)
+
+.PARAMETER NetbiosName
+    Nom NetBIOS du domaine (ex : NEOINFRA)
+
+.PARAMETER ForestMode
+    Niveau fonctionnel de la forêt (ex : WinThreshold, Win2016, Win2012R2)
+
+.PARAMETER DomainMode
+    Niveau fonctionnel du domaine (ex : WinThreshold, Win2016, Win2012R2)
+
+.PARAMETER InstallDNS
+    Active ou non le rôle DNS en même temps
+
+.PARAMETER DatabasePath
+    Chemin du fichier NTDS.dit
+
+.PARAMETER LogPath
+    Chemin des journaux NTDS
+
+.PARAMETER SysvolPath
+    Répertoire SYSVOL
+
+.EXAMPLE
+    .\Deploy-ADDSForest.ps1 -DomainName "neoinfra.fr" -NetbiosName "NEOINFRA"
+
+.NOTES
+    Auteur : Ton Nom  
+    GitHub : https://github.com/ton-compte/Deploy-ADDSForest
+    Nécessite une machine propre avec Windows Server et RSAT
+#>
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$DomainName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$NetbiosName,
+
+    [string]$ForestMode = "WinThreshold",
+    [string]$DomainMode = "WinThreshold",
+
+    [bool]$InstallDNS = $true,
+
+    [string]$DatabasePath = "C:\Windows\NTDS",
+    [string]$LogPath =
